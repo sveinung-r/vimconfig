118d7effe322d7173158a852c8412e6f0ecd3a44
sveinung-r <ssru@equinor.com>
2022-07-25 08:24:36 +0000

    Fix install script and use copy not symlink

--- install.sh
+++ install.sh
@@ -11,23 +11,27 @@ files=".vimrc.first .vimrc.last .vimrc.plugins"
 for file in $files; do
     if [ -f ${HOME}/$file ]; then
         echo "Moving existing $file -> $olddir/$file"
-        mv ${HOME}/$file $oldir/
+        mv ${HOME}/$file $olddir/
     fi
 
-    echo -e "Creating symlink to $(pwd)/$file in home directory\n"
-    ln -s  $(pwd)/$file ${HOME}/$file
+    echo -e "Installing $file in home directory\n"
+    cp  $(pwd)/$file ${HOME}/$file
 done
 
 if [ -f ${HOME}/.vimrc ]; then
     echo "Moving existing .vimrc -> $olddir/"
-    mv ${HOME}/.vimrc $oldir/
+    mv ${HOME}/.vimrc $olddir/
 fi
 
-echo "Fetching vimconfig: https://github.com/timss/vimconf.git"
-git clone https://github.com/timss/vimconf.git ~/vimconf
-git checkout 23a01b3bcbfc13bfebd61f1da6b50a36b6e29e91
+if [ ! -d $(pwd)/vimconf ]; then
+    echo "Fetching vimconfig: https://github.com/timss/vimconf.git"
+    git clone https://github.com/timss/vimconf.git
+    pushd vimconf
+    git checkout 23a01b3bcbfc13bfebd61f1da6b50a36b6e29e91
+    popd
+fi
 
-echo "Creating symlink to ~/vimconf/.vimrc in home directory."
-ln -s ${HOME}/vimconf/.vimrc ${HOME}/.vimrc
+echo "Installing .vimrc in home directory\n"
+cp $(pwd)/vimconf/.vimrc ${HOME}/.vimrc
 
 source $HOME/.bashrc
